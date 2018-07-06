276f959682793ee703ec9b95d1fc3d44a8fae47d
Miguel Ángel Cárdenas <makquel@gmail.com>
2018-07-06 13:53:46 -0300

    Create funcoes.h

---
+++ funcoes.h
@@ -0,0 +1,10 @@
+#ifndef FUNCOES_H
+#define FUNCOES_H
+#include <iostream>
+#include <vector>
+int factorial (int n);
+float expo (float x, int n);
+int PA_TotalTermos ( int inicial, int final, int razao );
+void ImprimePares ( int inicial , int final );
+
+#endif // FUNCOES_H
