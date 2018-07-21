d31648a17d0af0ecb61cf1ea23ac4f5797e963e0
Miguel Ángel Cárdenas <makquel@gmail.com>
2018-07-21 16:16:41 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,22 @@
 #include "data.h"
 using namespace std;
 
+std::vector<int> solution(std::vector<int> &A, int K);
+
 int main()
 {
     Data dt, dt1(1900,2100);
 
 //  Data::Altera(&dt,10,3,2018);
-    dt.altera(5,3,2018);
+    dt.altera(5,2,2018);
     dt.imprime();
 
 //  Data::Altera(&dt1,10,3,2018);
-    dt1.altera(32,3,2018);
-    dt1.imprime();
-
+    dt1.altera(5,3,2019);
+//    dt1.imprime();
+    std::cout << "dt = " << dt << '\n';
+    std::cout << "ano bissexto: " << dt1.anoBissexto() << std::endl;
+    std::cout << "Ultimo dia do mês: " << dt1.UltimoDiaMes()<< std::endl;
+    std::cout << "Comparação: " << dt.compara(dt1) << std::endl;
     return 0;
 }
