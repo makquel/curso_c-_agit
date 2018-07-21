1e471b66ba8da7b3a86d17cd7f45e9d8e583953b
Miguel Ángel Cárdenas <makquel@gmail.com>
2018-07-21 16:17:26 -0300

    Update data.h

--- data.h
+++ data.h
@@ -1,9 +1,25 @@
 #ifndef DATA_H
 #define DATA_H
-
+#include <iostream>
 class Data
 {
+    friend std::ostream &operator << (std::ostream &o, const Data &dt);
 private:
+    enum color
+    {
+        JANEIRO,
+        FEVEREIRO,
+        MARCO,
+        ABRIL,
+        MAIO,
+        JUNHO,
+        JULHO,
+        AGOSTO,
+        SETEMBRO,
+        OUTUBRO,
+        NOVEMBRO,
+        DEZEMBRO
+    };
     char m_dia, m_mes;
     short m_ano;
     short m_anoMin = 0, m_anoMax = 2999;
@@ -19,6 +35,13 @@ public:
     void altera(const char d, char m, short a);
     void imprime() const;
     bool isOk() { return m_bOk; }
+    bool anoBissexto() const; // only reading function
+    int UltimoDiaMes() const;
+    int compara(/*const Data *this,*/ const Data &dt) const;
+    inline bool operator == (const Data &dt) const;
+    inline bool operator < ( const Data &dt )const;
+    inline bool operator > ( const Data &dt )const;
+
 };
 
 #endif // DATA_H
